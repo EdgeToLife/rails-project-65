2749d73f1831b4722e301724297a3d62fec75e2e
Ivan Zhukov <ziv@servers.com>
2024-02-13 21:25:32 +0200

    preview-2

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -33,7 +33,7 @@ module Web
       existing_user = User.find_by(email: auth['info']['email'])
       email = auth[:info][:email].downcase
       name = auth[:info][:name]
-      name = name.empty? ? email : name
+      name = email if name.empty?
       user = existing_user || User.new(name:, email:)
 
       is_new = false
